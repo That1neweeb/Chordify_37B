957741a6428dbe2e4f338d0101ea7634425f3cb4
That1neweeb <sidhant.giri32@gmail.cpm>
2025-11-24 17:37:25 +0545

    Register Page added

---
+++ Chordify/src/RegisterCard.jsx
@@ -0,0 +1,29 @@
+import './RegisterCard.css'
+function RegisterCard(){
+    return(
+        <>
+        <div className="Title">
+                <h1>
+                    Join Chordify
+                </h1>
+                <p>Sign up today and discover, learn, and play like never before.</p>
+        </div>
+
+        <div className='Register-form'>
+            <form>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" />
+                <label htmlFor="email">Password</label>
+                <input type="password" name="password" id="password" />
+                <label htmlFor="c_password">Confirm password</label>
+                <input type="password" name="c_password" id="c_password" />
+                <button type="submit">Create account</button>
+                {/* <hr>or</hr>     */}
+            </form>
+            <img src="../src/assets/collage2.png" alt="login image"></img>
+        </div>
+        </>
+    )
+}
+
+export default RegisterCard
