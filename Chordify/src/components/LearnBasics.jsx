7792cf695a10884017fb4d3e3aac22f6f53d747c
Satyam Shrestha <52965655+mrxstm@users.noreply.github.com>
2025-11-29 09:34:14 +0545

    LearnBasics created and loaded the LearnBasicCard there

---
+++ Chordify/src/components/LearnBasics.jsx
@@ -0,0 +1,31 @@
+import BasicCard from "./BasicCard";
+import chordslibrary from "../assets/images/sliders.png";
+import tabexercise from "../assets/images/sheet-music.png";
+import strumming from "../assets/images/musical.png";
+
+
+const basiccards = [
+    {icon : chordslibrary , title: "Chords Library", description : "Learn some basic chords"},
+    {icon : tabexercise, title: "Tab exercises", description : "Learn fingerstyle"},
+    {icon : strumming, title: "Essential Strumming Patterns", description : "Master common strumming patterns"}
+] 
+function LearnBasics() {
+
+    return(
+        <div className="mt-32 ">
+            <h2 className="font-bold text-2xl">Learn Basics</h2>
+            <div className="grid grid-cols-3 mt-10">
+                {basiccards.map((basiccard,index)=>
+                    <BasicCard
+                        key={index}
+                        icon={basiccard.icon}
+                        title={basiccard.title}
+                        description={basiccard.description}
+                    />
+                 )}  
+            </div>
+        </div>
+    );
+}
+
+export default LearnBasics;
