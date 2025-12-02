b577e4074cb3bfa3f1b62b784033ce5806b25a53
That1neweeb <sidhant.giri32@gmail.cpm>
2025-12-02 08:42:36 +0545

    moved folders

--- Chordify/src/App.jsx
+++ Chordify/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import RegisterCard from "./RegisterCard"
-import Register from "./Register"
+import RegisterCard from "./components/RegisterCard"
+import Register from "./pages/Register"
 function App() {
   return (
     // <h2>Hello</h2>
