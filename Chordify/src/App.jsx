0a4b4e7753d56211a8abadfac92f4bf16a8106c0
Satyam Shrestha <52965655+mrxstm@users.noreply.github.com>
2025-12-19 11:08:33 +0545

    App.jsx is changed to add routes

--- Chordify/src/App.jsx
+++ Chordify/src/App.jsx
@@ -4,9 +4,22 @@ import Register from "./pages/Register"
 function App() {
   return (
     // <h2>Hello</h2>
-    <>
+   <>
+      <Navbar/>
+     
+      <div className="w-full h-px bg-[#777063] mt-3"></div> 
 
-    <Register></Register>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Sell" element={<Sale/>}></Route>
+        <Route path="/Buy" element={<Buy/>}></Route>
+        <Route path="/Learn" element={<Learn/>}></Route>
+        <Route path="/Aboutus" element={<Aboutus/>}></Route> 
+        <Route path="/register" element={<RegistrationPage/>}></Route> 
+        <Route path="/login" element={<LoginPage/>}></Route> 
+        <Route path="/verify/:token" element={<VerificationPage />} />
+      </Routes>  
+
     </>
     
   )
