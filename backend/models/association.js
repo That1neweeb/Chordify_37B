33027d3a287a47c805b4c79db86720a9da105f9f
Satyam Shrestha <52965655+mrxstm@users.noreply.github.com>
2026-01-04 09:16:14 +0545

    association defined in association.js for cart, cartItem and Products

--- backend/models/association.js
+++ backend/models/association.js
@@ -1,6 +1,10 @@
 import { Products } from "./productModel.js";
 import { GuitarDetails } from "./guitarDetailsModel.js";
+import { Users } from "./userModel.js"
+import { Cart } from "./cartModel.js";
+import { CartItem } from "./cartItemModel.js";
 
+//Product and GuitarDetails relationship
 Products.hasOne(GuitarDetails, {
     foreignKey: 'product_id',
     as: 'guitarDetails',
@@ -10,6 +14,38 @@ Products.hasOne(GuitarDetails, {
 GuitarDetails.belongsTo(Products, {
     foreignKey: 'product_id'
 })
+
+
+//Users and cart relationship
+Users.hasOne(Cart, {
+  foreignKey: 'user_id',
+  as: 'cart',
+  onDelete:'CASCADE'
+})
+
+Cart.belongsTo(Users, {
+  foreignKey: 'user_id'
+});
+
+//Cart and CartItem relationship
+CartItem.belongsTo(Cart, {
+  foreignKey: 'cart_id'
+})
+
+Cart.hasMany(CartItem, {
+  foreignKey: 'cart_id'
+})
+
+// Product and CartItem relationship
+Products.hasMany(CartItem, {
+  foreignKey: 'product_id'
+});
+
+CartItem.belongsTo(Products, {
+  foreignKey: 'product_id'
+});
+
+
 
 export {
   Products,
