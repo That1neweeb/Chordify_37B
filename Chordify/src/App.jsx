110f6a34113fe54c5e3711c0c8c537a919a9c7ef
Nitant Ghimire <ghimirenitant2006@gmail.com>
2026-01-19 10:57:31 +0545

    Update main app routing and layout

--- Chordify/src/App.jsx
+++ Chordify/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import RegisterCard from "./components/RegisterCard"
-import Register from "./pages/Register"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminLayout from './layouts/AdminLayout'; // <-- import layout
+import AdminDashboard from './pages/AdminDashboard';
+import ProductListing from './pages/ProductListing';
+import UserListing from './pages/UserListing';
+import GuitarListing from './pages/GuitarListing';
+
 function App() {
   return (
-    // <h2>Hello</h2>
-    <>
-
-    <Register></Register>
-    </>
-    
-  )
+      <Routes>
+        <Route element={<AdminLayout />}>
+          <Route path="/" element={<AdminDashboard />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/users" element={<UserListing />} />
+          <Route path="/guitars" element={<GuitarListing />} />
+        </Route>
+      </Routes>
+  );
 }
 
-export default App
+export default App;
