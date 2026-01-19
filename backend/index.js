90317d71f8133a8a85de80b8b4eb8a1e3985bdf2
Nitant Ghimire <ghimirenitant2006@gmail.com>
2026-01-19 11:00:33 +0545

    Update backend server entry point

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,29 @@ app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-
+//Guitar routes
 //to fetch guitars
-const guitarRouter = require("./routes/guitars");
-app.use('/guitars', guitarRouter);
+const guitarRouter = require("./routes/guitar");
+app.use('/guitar', guitarRouter);
 
+
+//Product routes
+//to fetch products
+const productRouter = require("./routes/product");
+app.use('/product', productRouter);
+
+//User routes
+const userRouter = require("./routes/user");
+app.use('/users', userRouter);
+
+
+//Songs(if any)
 //to fetch songs
 const songRouter = require("./routes/songs");
 app.use('/songs', songRouter);
 
-app.listen(5000);
+
+app.listen(5000, () =>
+    console.log("server running on port 5000")
+);
 
