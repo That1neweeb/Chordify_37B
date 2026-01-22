6b315f7892b026d1ddc94ca382a15d5a0a4bfad3
Satyam Shrestha <52965655+mrxstm@users.noreply.github.com>
2026-01-22 09:13:57 +0545

    refactor(frontend/api): replace direct API calls with custom hook

--- Chordify/src/components/InputField.jsx
+++ Chordify/src/components/InputField.jsx
@@ -1,50 +1,51 @@
 import google from '../assets/images/google.png'
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import {zodResolver} from "@hookform/resolvers/zod"
 import { useNavigate } from 'react-router-dom';
+import { useApi } from "../hooks/useAPI.js"; 
+import { toast } from 'react-toastify';
+import { registerSchema, loginSchema } from "../schema/register.schema";
+
+
 
 function InputField({isRegistration = true}) {
     
+
     const navigate = useNavigate();
-    const [full_name, setFullname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [c_password, setCPassword] = useState("");
 
-    const handleRegistration = async(e) => {
-        e.preventDefault();
+    //custom hook 
+    const { loading, error, callApi } = useApi();
+
+    //react hook form
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm(
+       { resolver:zodResolver(isRegistration? registerSchema: loginSchema)}
+    );
+
+    const onSubmit = async (formData) => {
         try {
-            const endpoint = isRegistration ? "register" : "login"
-            const bodyData = isRegistration
-                ? { full_name, email, password, c_password }
-                : { email, password };
+            const endpoint = isRegistration ? "/auth/register" : "/auth/login";
+            const data = await callApi("POST", endpoint, { data: formData });
 
-            const response = await fetch(`http://localhost:5000/auth/${endpoint}`, {
-                method: "POST",
-                headers: {"Content-Type" : "application/json"},
-                body: JSON.stringify(bodyData),
-            });
-            const data = await response.json();
-
-             if (response.ok) {
-           
-                
-                 if (isRegistration) {
-                        alert('Registration successful! Please check your email to verify your account before logging in.');
-                        navigate('/login');
-                 } else {
-                         alert(data.message);
-                        navigate('/');
-                    }
+            if (isRegistration) {
+                toast.success(
+                    "Registration successful! Please check your email to verify your account."
+                );
+                navigate("/login");
             } else {
-                alert(data.message);
+                localStorage.setItem("token", data.accessToken);
+                toast.success(data.message || "Login successful");
+                navigate("/");
             }
-            
-        } catch(err) {
-            console.log("Frontend error : ", err);
-            alert("Something went wrong")
-            
+        } catch (err) {
+            console.log("API error:", err.message);
+            toast.error(err.message);
         }
-    }
+    };
+
 
     return( 
     <div className="flex flex-col">
@@ -55,33 +56,33 @@ function InputField({isRegistration = true}) {
                     <input
                         type="text"
                         placeholder="fullname"
-                        name="fullname"
+                        {...register("fullname")}
                         className="w-[400px] h-[50px] border border-black rounded-md bg-white text-black text-[20px] my-4 px-2"
-                        value={full_name}
-                        onChange={(e) => setFullname(e.target.value)}
+                       
                     />
+                    {errors.fullname && (
+                        <p className="text-red-500 text-sm">{errors.fullname.message}</p>
+                    )}
                 </>
             )}
 
             <label htmlFor="email" className='text-black'>Email</label>
             <input 
-            type="email" 
-            placeholder="Email" 
-            name='email'
-            className="w-[400px] h-[50px] border border-black rounded-md bg-white text-black text-[20px] my-4 px-2" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Email"
+                {...register("email")}
+                className="w-[400px] h-[50px] border border-black rounded-md bg-white text-black text-[20px] my-4 px-2"
             />
+            {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
 
             <label htmlFor="password" className='text-black'>Password</label>
-            <input 
-            type="password" 
-            placeholder="Password" 
-            name='password' 
-            className="w-[400px] h-[50px] border border-black rounded-md bg-white text-black text-[20px] my-4 px-2" 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            <input
+                type="password"
+                placeholder="Password"
+                {...register("password")}
+                className="w-[400px] h-[50px] border border-black rounded-md bg-white text-black text-[20px] my-4 px-2"
             />
+            {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
 
             {isRegistration && (
                 <>
@@ -89,19 +90,19 @@ function InputField({isRegistration = true}) {
                     <input
                         type="password"
                         placeholder="Confirm Password"
-                        name="c_password"
+                        {...register("c_password")}
                         className="w-[400px] h-[50px] border border-black rounded-md bg-white text-black text-[20px] my-4 px-2"
-                        value={c_password}
-                        onChange={(e) => setCPassword(e.target.value)}
                     />
+                    {errors.c_password && <p className="text-red-500 text-sm">{errors.c_password.message}</p>}
                 </>
             )}
 
             <button
+                disabled={loading}
                 className="w-[400px] h-[50px] border border-black rounded-md bg-[#364737] text-[20px] mt-7"
-                onClick={handleRegistration}
+                onClick={handleSubmit(onSubmit)}
             >
-                {isRegistration ? 'Create account' : 'Login'}
+                {loading? "Please wait...." : isRegistration ? 'Create account' : 'Login'}
             </button>
 
             {isRegistration && (
@@ -112,12 +113,13 @@ function InputField({isRegistration = true}) {
                         <div className="flex-grow border-t border-gray-300"></div>
                     </div>
 
-                    <button className="flex items-center justify-center w-[400px] h-[50px] gap-2 border border-black rounded-md bg-[#E0E0E0] text-black mt-1">
+                    <button disabled={loading} className="flex items-center justify-center w-[400px] h-[50px] gap-2 border border-black rounded-md bg-[#E0E0E0] text-black mt-1">
                         <img src={google} alt="" className="h-4" />
                         Sign up with Google
                     </button>
                 </>
             )}
+
 
         </div>
         );
--- Chordify/src/pages/Buy.jsx
+++ Chordify/src/pages/Buy.jsx
@@ -1,108 +1,99 @@
 import GuitarCard from "../components/GuitarCard";
-import acoustic from "../assets/images/acoustic.png";
 import search from "../assets/images/search.png";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import { useApi } from "../hooks/useAPI.js";
 
 function Buy() {
+    const { loading, error, callApi } = useApi();
 
-    //for products data
     const [products, setProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
-    //for search texts
-    const[searchTerm, setSearchTerm] = useState("");
+    // Use useRef for debounce to persist timeout across renders
+    const debounceRef = useRef(null);
 
-    async function fetchProducts() {
+    // Fetch all products
+    const fetchProducts = async () => {
         try {
-            const response = await fetch("http://localhost:5000/products/buy"); //calling the backend api to getch the products
-            const result = await response.json();
-            setProducts(result.data);
-        } catch(err) {
-            console.log("Fetch error : "+err);
+            const data = await callApi("GET", "/products/buy");
+            setProducts(data);
+        } catch (err) {
+            console.error("Fetch products error:", err.message);
+        }
+    };
+
+    // Fetch products by search query
+    const fetchSearchResults = async (query) => {
+        if (!query) {
+            fetchProducts(); // If search is empty, fetch all
+            return;
         }
 
-    }
-    //to fetch the products
-    useEffect(()=>{        
-        fetchProducts();
-    }, [])
+        try {
+            const data = await callApi("GET", `/products/search`, {
+                params: { search: query },
+            });
+            setProducts(data);
+        } catch (err) {
+            console.error("Search error:", err.message);
+        }
+    };
 
-    // for debouncing (when the user stops typing , it calls the search api)
-    let debounceTimeout;
+    // Search input change with debounce
     const handleSearchChange = (e) => {
         const value = e.target.value;
         setSearchTerm(value);
 
         // Clear previous timeout
-        if (debounceTimeout) clearTimeout(debounceTimeout);
+        if (debounceRef.current) clearTimeout(debounceRef.current);
 
         // Set new timeout
-        debounceTimeout = setTimeout(() => {
-            //here im passing the value to the function fetchSearchResults
+        debounceRef.current = setTimeout(() => {
             fetchSearchResults(value);
-        }, 500); // wait 500ms after user stops typing
+        }, 500);
     };
 
-    //for fetching the actual product which was searched
-    const fetchSearchResults = async (query) => {
+    // Fetch products on component mount
+    useEffect(() => {
+        fetchProducts();
+    }, []);
 
-        //if search field is empty return all the products
-        if(!query) {
-            fetchProducts();
-            return;
-        }
+    return (
+        <div>
+            {/* Search area */}
+            <div className="flex items-center m-10 gap-2 bg-[#393328] border border-[#374151] rounded-xl hover:border-[#393328]">
+                <img src={search} alt="" className="size-6 ml-4" />
+                <input
+                    type="text"
+                    placeholder="Search product by name ...."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    className="w-full m-2 h-10 focus:outline-none bg-[#393328] text-white"
+                />
+            </div>
 
-        try {
-            const res = await fetch(`http://localhost:5000/products/search?search=${query}`);
-            const result = await res.json();
-            setProducts(result);
-        } catch(e) {
-            console.error("Search error:", err);
-        }
-    }
+            <div className="flex justify-around mr-8 mt-20">
+                <div className="bg-[#393328] w-64 h-[800px] rounded-2xl ml-5"></div>
 
-
-
-    //UI part
-    return(
-    <div>
-
-        {/* search area */}
-        <div className="flex items-center m-10 gap-2 bg-[#393328] border border-[#374151] rounded-xl hover:border-[#393328]">
-            <img src={search} alt="" className='size-6 ml-4'/>
-            <input 
-            onChange={handleSearchChange}
-            type="text" 
-            placeholder='Search product by name ....'
-            className='w-full m-2 h-10 focus:outline-none bg-[#393328] text-white'
-            />
-        </div>
-        <div className="flex justify-around mr-8 mt-20">
-
-            <div className="bg-[#393328] w-64 h-[800px] rounded-2xl ml-5"></div>
-
-            <div className="ml-12">
-                <h1 className="text-4xl font-bold">Products</h1>
-                <div className="grid grid-cols-3 gap-y-20 gap-x-20 mt-4">
-                    {products.map((product) => (
-                        <GuitarCard
-                            key={product.id}
-                            id={product.id}
-                            guitarName= {product.name}
-                            image={`http://localhost:5000${product.image_urls[0]}`}
-                            price= {product.price}
-                            rating= {product.rating}
-                            brand= {product.brand}
-                            page= "buying"
-                         />
-                    ))}
-               
-                    
-                 
+                <div className="ml-12">
+                    <h1 className="text-4xl font-bold">Products</h1>
+                    <div className="grid grid-cols-3 gap-y-20 gap-x-20 mt-4">
+                        {products.map((product) => (
+                            <GuitarCard
+                                key={product.id}
+                                id={product.id}
+                                guitarName={product.name}
+                                image={`http://localhost:5000${product.image_urls[0]}`}
+                                price={product.price}
+                                rating={product.rating}
+                                brand={product.brand}
+                                page="buying"
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
-
-    </div>
     );
 }
 
