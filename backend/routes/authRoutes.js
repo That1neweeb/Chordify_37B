5d57af3b2a11187262a7bb473f089587092335b9
Satyam Shrestha <52965655+mrxstm@users.noreply.github.com>
2026-01-22 09:04:26 +0545

    feat(auth): add auth middleware and protect routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import { isAuthenticated } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 // Controller
 import { registerUser, login, verifyUser } from '../controllers/authController.js';
 
 // Routes
+router.post('/me', isAuthenticated, (req,res) => {
+    res.status(200).json({
+        user: req.user
+    });
+});
 router.post('/register', registerUser);
 router.post('/login', login);
 router.get('/verify/:token', verifyUser);
